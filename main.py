d1016b4800892874ac6dd74ce0a3089709399c37
laura <laura.miarka@edu.uekat.pl>
2021-12-18 15:56:35 +0100

    poprawka

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from ratings import *
 app = Flask(__name__)
 api = Api(app)
 
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
 api.add_resource(Movie, '/movies')
 api.add_resource(Link, '/links')
 api.add_resource(Rating, '/ratings')
